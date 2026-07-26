c3c786d1ad6de321db392e0fb0041ab985b076e1
maroonooram5-ctrl <maroonooram5@daum.net>
2026-07-26 14:42:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const airports = {
+  "간사이": ["KIX", "ITM", "UKB"],
+  "간토": ["NRT", "HND"],
+  "주부": ["NGO", "FSZ"],
+  "홋카이도": ["CTS", "HKD"],
+  "주고쿠": ["HIJ", "YGJ"],
+  "시코쿠": ["TAK", "MYJ", "TKS"],
+  "큐슈": ["FUK", "KKJ", "KMJ", "KOJ"],
+  "오키나와": ["OKA"]
+};
+
 function searchFlight() {
     document.getElementById("result").style.display = "block";
 }
